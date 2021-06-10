8896e7aa75c85e896e4393398e3c3a1e089abf76
Lucas Augusto de Morais <lucasm310@gmail.com>
2021-06-10 20:05:26 -0300

    Add links to courses

--- src/pages/CentrosFormadores/index.js
+++ src/pages/CentrosFormadores/index.js
@@ -9,6 +9,49 @@ import logo_dantepp from "../../assets/logo_dantepp.png";
 import logo_unicamp from "../../assets/logo_unicamp.png";
 import logo_unifesp from "../../assets/logo_unifesp.png";
 
+const centros = [
+  {
+    name: "ipessp",
+    url: "http://www.ipessp.edu.br/site/cursos/extracorporea-e-assistencia-circulatoria-mecanica/",
+    logo: logo_ipessp,
+  },
+  {
+    name: "issec",
+    url: "http://www.iseec.com.br/circula%C3%A7%C3%A3o-extracorp%C3%B3rea-e-suporte-de-vida-avan%C3%A7ado.html",
+    logo: logo_issec,
+  },
+  {
+    name: "hcm",
+    url: "http://www.hcmriopreto.com.br/",
+    logo: logo_hcm,
+  },
+  {
+    name: "cec_inc_rj",
+    url: "https://sbcec.com.br/br/index.php/congresso-cursos-eventos/centros-formadores/44-training/centros-perfusao-brasil/240-curso-de-circulacao-extracorporea.html",
+    logo: logo_cec_inc_rj,
+  },
+  {
+    name: "incor",
+    url: "https://referenciaincor.com.br/cursos/especializacao-em-circulacao-extracorporea-e-assistencia-circulatoria-mecanica/",
+    logo: logo_incor,
+  },
+  {
+    name: "dantepp",
+    url: "http://www.idpc.org.br/?Fuseaction=Home",
+    logo: logo_dantepp,
+  },
+  {
+    name: "unicamp",
+    url: "https://sbcec.com.br/br/index.php/congresso-cursos-eventos/centros-formadores/44-training/centros-perfusao-brasil/89-cursos-escola-de-extensao-unicamp-campinas-perfusao-enfermagem-biomedico.html",
+    logo: logo_unicamp,
+  },
+  {
+    name: "unifesp",
+    url: "https://sp.unifesp.br/epm/cardiovascular/cursos/perfusao",
+    logo: logo_unifesp,
+  },
+];
+
 function CentrosFormadores() {
   return (
     <section id="centros-formadores" className="sections">
@@ -18,46 +61,17 @@ function CentrosFormadores() {
         </Col>
       </Row>
       <Row className="lista-centros-formadores">
-        <Col md={3} className="d-flex justify-content-center mb-5">
-          <div className="lista-item">
-            <Image src={logo_ipessp} className="ipessp" />
-          </div>
-        </Col>
-        <Col md={3} className="d-flex justify-content-center mb-5">
-          <div className="lista-item">
-            <Image src={logo_issec} className="issec" />
-          </div>
-        </Col>
-        <Col md={3} className="d-flex justify-content-center mb-5">
-          <div className="lista-item">
-            <Image src={logo_hcm} className="hcm" />
-          </div>
-        </Col>
-        <Col md={3} className="d-flex justify-content-center mb-5">
-          <div className="lista-item ">
-            <Image src={logo_cec_inc_rj} className="cec_inc_rj" />
-          </div>
-        </Col>
-        <Col md={3} className="d-flex justify-content-center mb-5">
-          <div className="lista-item ">
-            <Image src={logo_incor} className="incor" />
-          </div>
-        </Col>
-        <Col md={3} className="d-flex justify-content-center mb-5">
-          <div className="lista-item ">
-            <Image src={logo_dantepp} className="dantepp" />
-          </div>
-        </Col>
-        <Col md={3} className="d-flex justify-content-center mb-5">
-          <div className="lista-item ">
-            <Image src={logo_unicamp} className="unicamp" />
-          </div>
-        </Col>
-        <Col md={3} className="d-flex justify-content-center mb-5">
-          <div className="lista-item ">
-            <Image src={logo_unifesp} className="unifesp" />
-          </div>
-        </Col>
+        {centros.map((centro, idx) => (
+          <Col key={idx} md={3} className="d-flex justify-content-center mb-5">
+            <div className="lista-item">
+              <Image
+                src={centro.logo}
+                className={centro.name}
+                onClick={() => window.open(centro.url, "_blank")}
+              />
+            </div>
+          </Col>
+        ))}
       </Row>
     </section>
   );
